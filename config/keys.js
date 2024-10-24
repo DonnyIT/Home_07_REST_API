029b5004021f628d5a0e7cd58ed8379c344c2cba
Sasha Mushenko <mcasha888@gmail.com>
2024-10-24 20:23:14 +0300

    Connection script

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUrl: 'mongodb+srv://mcasha888:5jD5A6ufIXfpeDtQ@cluster0.h3czg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    mongoUrl: '____', // connectionScript
     Jwt: '777'
 }
